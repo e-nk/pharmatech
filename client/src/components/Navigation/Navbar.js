c48326c6a25bdbaf1bfab26da847fd369159fe47
e-nk <enocklagat747@gmail.com>
2023-05-27 18:15:11 +0300

    navigation

---
+++ client/src/components/Navigation/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, useHistory } from 'react-router-dom';
+import './Navbar.css';
+
+
+function Navbar() {
+  
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
+        <div className="container-fluid">
+          <ul className="navbar-nav">
+            <li className="nav-item logo nav-header custom-link">
+              <Link to="/">Pharmatech</Link>
+            </li>
+          </ul>
+          <ul className="nav nav-pills margin-right">
+            <li className="nav-item custom-link">
+            </li>
+            <li className="nav-item ms-1 custom-link">
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+    </>
+  );
+}
+
+export default Navbar;
